354c2ab6eae760c908ba15ffe1f34dee9ac38e88
RaviChauhan <1chauhanravi0@gmail.com>
2026-07-18 11:17:14 +0530

    Prepare frontend for deployment

--- Brainly/src/index.ts
+++ Brainly/src/index.ts
@@ -5,7 +5,12 @@ import contentRouter from "./routes/content.js";
 import shareRouter from "./routes/share.js";
 import cors from "cors";
 const app = express();
-app.use(cors())
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://YOUR-VERCEL-DOMAIN.vercel.app"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/api/v1/user", userRouter);
